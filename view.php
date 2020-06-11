07cd2571b4fca0445e227bf41e4db575e77bdb57
SoupCake <61450397+SoupCake@users.noreply.github.com>
2020-06-11 14:46:46 +0800

    Update view.php

--- view.php
+++ view.php
@@ -11,17 +11,8 @@
   //echo "Connection Success: connected!";
   // displaying table
   echo "<table border='1'>";
-    $tsql = "SELECT * FROM [dbo].[restaurant]";
-    $getResults = sq;srv_query($conn, $tsql);
-    if ($getResults == FALSE)
-    {
-      die(sqlsrv_errors());
-    }
-    while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC))
-    {
-      echo "<tr>";
-      echo "<td>".$row['restaurant_id']."</td>";
-      echo "</tr>";
-    }
+    $tsql= "SELECT * FROM [dbo].[restaurant]";        $getResults= sqlsrv_query($conn, $tsql);                           if ($getResults == FALSE)  {             die(sqlsrv_errors());   }                          
+ 
+       while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC))   {             echo "<tr>";             echo "<td>". $row['restaurant_id'] . "</td>";             echo "<td>". $row['restaurant_name'] ."</td>";             echo "<td>". $row['restaurant_address'] . "</td>";             echo "<td>". $row['restaurant_phone'] . "</td>";             echo "</tr>";         }        sqlsrv_free_stmt($getResults)
   echo "</table>";
 ?>
